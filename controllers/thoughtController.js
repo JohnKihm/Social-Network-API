8f487d51753cd7e683ef4d1cc280e74ca9c0040d
JohnKihm <john.a.kihm@gmail.com>
2024-08-05 00:40:52 -0500

    laid out basic structure of userController.js and thoughtController.js

---
+++ controllers/thoughtController.js
@@ -0,0 +1,60 @@
+//
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async createThought(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async getSingleThought(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async updateThought(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async deleteThought(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async addReaction(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async removeReaction(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+};
---
+++ controllers/userController.js
@@ -0,0 +1,60 @@
+//
+
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async createUser(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async getSingleUser(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async updateUser(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async addFriend(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async removeFriend(req, res) {
+        try {
+            //
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+};
