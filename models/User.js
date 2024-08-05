a25ac4b3c23a38af119cbef0132a7f1748e03fdc
JohnKihm <john.a.kihm@gmail.com>
2024-08-04 23:40:16 -0500

    added User model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: function(v) {
+                    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+                },
+                message: 'Email invalid'
+            },
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [this],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    },
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
