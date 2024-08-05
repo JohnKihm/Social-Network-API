b6fe6a71c85c7535bcd2375fb8b17a9e4f8de330
JohnKihm <john.a.kihm@gmail.com>
2024-08-05 00:46:51 -0500

    filled in getUsers controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,11 @@
-//
+const { User } = require('../models');
 
 module.exports = {
     async getUsers(req, res) {
         try {
-            //
+            const users = await User.find();
+
+            res.json(users);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
