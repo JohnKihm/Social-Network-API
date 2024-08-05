1b219da16ea7e6b3c68a82452c38ce1c4ebe5ef4
JohnKihm <john.a.kihm@gmail.com>
2024-08-05 00:48:29 -0500

    filled in createUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,6 @@ module.exports = {
     async getUsers(req, res) {
         try {
             const users = await User.find();
-
             res.json(users);
         } catch (err) {
             console.log(err);
@@ -13,7 +12,8 @@ module.exports = {
     },
     async createUser(req, res) {
         try {
-            //
+            const user = await User.create(req.body);
+            res.json(user);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
